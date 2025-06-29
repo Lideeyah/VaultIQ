45b96a40f5e8d3ade4ae5f4b50254da7cf864ed3
ladicodes <ladiposamuel30@gmail.com>
2025-06-29 23:54:01 +0100

    m

--- src/VaultIQ_backend/api/prisma/prisma copy/migrations/20250629210445_init/migration.sql
+++
@@ -1,9 +0,0 @@
--- CreateTable
-CREATE TABLE "Asset" (
-    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-    "fileUrl" TEXT NOT NULL,
-    "metadata" JSONB NOT NULL,
-    "status" TEXT NOT NULL,
-    "score" REAL NOT NULL,
-    "owner" TEXT NOT NULL
-);
