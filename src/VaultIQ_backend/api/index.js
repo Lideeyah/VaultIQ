2bd9e10d3b26900ad440603336e68add19dd9d13
ladicodes <ladiposamuel30@gmail.com>
2025-07-09 23:22:54 +0100

    final commit

--- src/VaultIQ_backend/api/index.js
+++ src/VaultIQ_backend/api/index.js
@@ -55,11 +55,6 @@ import { Web3Storage, File } from "web3.storage";
 
 const client = new Web3Storage({ token: process.env.WEB3_STORAGE_KEY });
 
-export async function uploadToIPFS(file) {
-  const fileToUpload = new File([file.buffer], file.originalname);
-  const cid = await client.put([fileToUpload]);
-  return `https://${cid}.ipfs.w3s.link/${file.originalname}`;
-}
 
 // utils/verifier.js
 import OpenAI from "openai";
